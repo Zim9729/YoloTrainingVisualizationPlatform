ea2123b2cddad57be4f8bfc465f1e0967f9ae0ea
chzane <986686113@qq.com>
2025-07-12 11:45:04 +0800

    chore:补充提交d41036f，添加了用于合并路径的pathJoin方法

---
+++ frontend/src/tools.js
@@ -0,0 +1,16 @@
+function pathJoin(...parts) {
+    const isWindows = navigator?.platform?.startsWith('Win'); // 这个声明已经弃用，后续考虑更换方案
+    const separator = isWindows ? '\\' : '/';
+
+    return parts
+        .map((part, index) => {
+            if (index === 0) return part.replace(/[\\/]+$/, '');
+            return part.replace(/^[\\/]+|[\\/]+$/g, '');
+        })
+        .filter(Boolean)
+        .join(separator);
+}
+
+export {
+    pathJoin
+};
