e4e65117e96234ad5954d4478e62059c5c07587d
chzane <986686113@qq.com>
2025-07-13 13:09:23 +0800

    perf:修改了HomePage的UI布局
    - 将Banner调整为圆角，并删除了描述文本
    - 删除了"快速开始"Card-group的的按钮，改为点击卡片直接跳转
    - 添加了"进行中的训练"板块，用于显示当前正在运行的训练任务并可点击跳转

--- frontend/src/page/HomePage.jsx
+++ frontend/src/page/HomePage.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
+import { api } from "../api";
+
 import BACKGROUND_IMAGE from "@/assets/background_3.png";
+import Icon_rocket_takeoff from "../assets/icons/rocket-takeoff-fill.svg";
 
 function HomePage({ setPageUrl, parameter }) {
+    const [runningTasksList, setRunningTasksList] = useState([]);
+
+    useEffect(() => {
+        api.get("/ITraining/getAllRunningTasks", { params: {} })
+            .then(data => {
+                console.log("获取正在运行的训练任务:", data.data.tasks);
+                setRunningTasksList(data.data.tasks);
+            })
+            .catch(err => {
+                console.error("获取正在运行的训练任务失败:", err);
+                alert(err);
+            });
+    }, [])
+
     return (
-        <div className="main nopadding">
+        <div className="main">
             <div className="banner" style={{
                 backgroundImage: `url(${BACKGROUND_IMAGE})`,
                 backgroundSize: 'cover',
@@ -10,35 +28,51 @@ function HomePage({ setPageUrl, parameter }) {
                 backgroundPosition: 'center',
             }}>
                 <h1 className="page-title">👋 欢迎</h1>
-                <p className="page-des">快速开始训练视觉模型</p>
             </div>
-            <div style={{ padding: '40px 70px' }}>
-                <div className="card-group" style={{ marginTop: '10px' }}>
-                    <div className="card pointer">
+
+            <div>
+                <h1 className="home-page-title">快速开始</h1>
+                <div className="card-group" style={{ marginTop: '20px' }}>
+                    <div className="card b-transform pointer" onClick={() => { setPageUrl("dataset?type=uploadDataset") }}>
                         <div className="top-tag">1</div>
                         <h1 className="title">上传数据集</h1>
                         <p className="content">支持 YOLO / COCO 格式的数据集，系统会自动解析标注信息并生成预览。</p>
-                        <button className="btn sm bottom-btn" onClick={() => { setPageUrl("dataset?type=uploadDataset") }} style={{ marginRight: '5px' }}>上传数据集</button>
-                        <button className="btn sm bottom-btn" onClick={() => { setPageUrl("dataset") }}>从已有的数据集开始</button>
                     </div>
-                    <div className="card pointer">
+                    <div className="card b-transform pointer" onClick={() => { setPageUrl("tasks?type=newTask") }}>
                         <div className="top-tag">2</div>
-                        <h1 className="title">配置训练任务</h1>
+                        <h1 className="title">创建训练任务</h1>
                         <p className="content">选择基础模型，并设置 epoch、batch size、图像尺寸等参数，无需编码，即可轻松完成配置。</p>
-                        <button className="btn sm bottom-btn" onClick={() => { setPageUrl("tasks?type=newTask") }}>创建训练任务</button>
                     </div>
-                    <div className="card pointer">
+                    <div className="card b-transform pointer" onClick={() => { setPageUrl("tasks") }}>
                         <div className="top-tag">3</div>
-                        <h1 className="title">一键启动模型训练</h1>
+                        <h1 className="title">启动模型训练</h1>
                         <p className="content">点击“开始训练”，系统将在后台自动调度任务，实时跟踪训练进度。</p>
-                        <button className="btn sm bottom-btn" onClick={() => { setPageUrl("tasks") }}>查看我的任务</button>
                     </div>
-                    <div className="card pointer">
+                    <div className="card b-transform pointer" onClick={() => { setPageUrl("models?type=trained") }}>
                         <div className="top-tag">4</div>
                         <h1 className="title">下载模型并测试</h1>
                         <p className="content">训练完成后可一键下载模型文件，同时可查看模型在验证集上的预测结果。</p>
-                        <button className="btn sm bottom-btn" onClick={() => { setPageUrl("models?type=trained") }}>查看我的模型</button>
                     </div>
+                </div>
+
+                <div style={{ marginTop: '40px' }}>
+                    <h1 className="home-page-title">进行中的训练</h1>
+                    {runningTasksList.length <= 0 ? (
+                        <span style={{ color: 'var(--secondary-text-color)' }}>空空如也</span>
+                    ) : (
+                        <div className="list-card-group">
+                            {runningTasksList.map((item, index) => (
+                                <div key={`runningTaskList_${index}`} className="list-card" style={{ wordBreak: 'break-all' }} onClick={() => setPageUrl(`tasksDetailed?filename=${item.filename}`)}>
+                                    <span style={{ fontSize: '18px', fontWeight: 'bold', marginRight: '4px' }}>
+                                        {item.taskname}
+                                    </span>
+                                    <span style={{ fontSize: '14px' }}>
+                                        #{item.task_id}
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
